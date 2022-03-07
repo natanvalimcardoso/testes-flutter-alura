da4353c44400891b9487303bc4ce24aeb1d27600
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-03-07 10:39:17 -0300

    refatorado

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -5,14 +5,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Deve mostrar na main a imagem quando o dashboard está aberto', (WidgetTester tester) async {
+  testWidgets('Deve mostrar na main a imagem quando o dashboard está aberto',
+      (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final mainImage = find.byType(Image); // SERVE PARA BUSCAR O ELEMENTO NA TELA E VER SE ELE ESTÁ VISIVEL OU NÃO
+    final mainImage = find.byType(
+        Image); // SERVE PARA BUSCAR O ELEMENTO NA TELA E VER SE ELE ESTÁ VISIVEL OU NÃO
     expect(mainImage, findsNWidgets(1)); // VERIFICA SE ACHOU 1 ELEMENTO
   });
-  testWidgets('Deve mostrar a primeira feature quando o dashboard está aberto', (tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
-    final firstFeature = find.byType(FeatureItem); // SERVE PARA BUSCAR O ELEMENTO NA TELA E VER SE ELE ESTÁ VISIVEL OU NÃO
-    expect(firstFeature, findsNWidgets(2)); // VERIFICA SE ACHOU 2 ELEMENTOS
-  });
+  testWidgets(
+    'Deve mostrar a funcionalidade de tranfer quando o dashboard está aberto',
+    (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Dashboard()));
+      (tester) async {
+        final transferFeatureItem = find.byWidgetPredicate((widget) =>
+            featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
+        expect(transferFeatureItem, findsOneWidget);
+      };
+    },
+  );
+  testWidgets(
+    'Deve mostrar a funcionalidade de transaction feed quando o dashboard está aberto*',
+    (tester) async {
+      await tester.pumpWidget(MaterialApp(home: Dashboard()));
+      (tester) async {
+        final transferFeatureItem = find.byWidgetPredicate((widget) =>
+            featureItemMatcher(widget, 'Transaction Feed', Icons.description)); // Função está sendo chamada a baixo
+        expect(transferFeatureItem, findsOneWidget);
+      };
+    },
+  );
+}
+
+
+
+//* Funções
+bool featureItemMatcher(Widget widget, String name, IconData icon) { // FUNÇÃO PARA VERIFICAR SE O WIDGET É O QUE EU QUERO, PARA NÃO TER QUE REPETIR O CÓDIGO
+  return widget is FeatureItem &&
+          widget.title == name &&
+          widget.icons == icon;
 }
